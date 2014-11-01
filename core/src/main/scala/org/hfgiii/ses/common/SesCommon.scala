74070eca95d32427bb4c8e40972e8c9e07212928
hgilmore <hgilmore@underarmour.com>
2014-11-01 08:11:51 -0400

    moved CSVParsers into SesCommon where it should have been in the first place

--- core/src/main/scala/org/hfgiii/ses/common/SesCommon.scala
+++ core/src/main/scala/org/hfgiii/ses/common/SesCommon.scala
@@ -1,6 +1,7 @@
 package org.hfgiii.ses.common
 
 import org.hfgiii.ses.common.admin.EsAdmin
+import org.hfgiii.ses.common.csv.parser.CSVParsboiledParsers
 import org.hfgiii.ses.common.dsl.response.readers.ResponseReaderDsl
 import org.hfgiii.ses.common.load.EsIndexLoad
 
@@ -8,4 +9,5 @@ import org.hfgiii.ses.common.load.EsIndexLoad
 object SesCommon
        extends EsAdmin
        with    EsIndexLoad
+       with    CSVParsboiledParsers
        with    ResponseReaderDsl
--- core/src/main/scala/org/hfgiii/ses/common/csv/parser/CSVParboiledParserSB.scala
+++ core/src/main/scala/org/hfgiii/ses/common/csv/parser/CSVParboiledParserSB.scala
@@ -31,42 +31,72 @@ import org.parboiled2.{CharPredicate, StringBuilding, Parser}
  * @tparam O - The result type of each line parse
  */
 
-trait CSVParboiledParserSB[O] extends Parser with StringBuilding {
-  val formatter = new SimpleDateFormat("yyyy-MM-dd")
+trait CSVParsboiledParsers {
 
-  /* start of csv parser */
-  def csvfile = rule{ (hdr ~ zeroOrMore(row)) ~> makeListOfOutputValues ~ zeroOrMore(optional("\r") ~ "\n") ~ EOI}
-  def hdr = rule{ row }
-  def row = rule{ oneOrMore(field).separatedBy(",") ~> genOutput ~ optional("\r") ~ "\n" }
-  def field = rule{ string | text | MATCH ~> makeEmpty }
-  def text = rule{ clearSB() ~ oneOrMore(noneOf(",\"\n\r")~ appendSB()) ~ push(sb.toString) ~> makeText }
-  def string = rule{ WS ~ "\"" ~ clearSB() ~ zeroOrMore(("\"\"" | noneOf("\"")) ~ appendSB()) ~ push(sb.toString) ~> makeString ~ "\"" ~ WS }
+  trait CSVParboiledParserSB[O] extends Parser with StringBuilding {
+    val formatter = new SimpleDateFormat("yyyy-MM-dd")
 
-  val whitespace = CharPredicate(" \t")
-  def WS = rule{ zeroOrMore(whitespace) }
+    /* start of csv parser */
+    def csvfile = rule {
+      (hdr ~ zeroOrMore(row)) ~> makeListOfOutputValues ~ zeroOrMore(optional("\r") ~ "\n") ~ EOI
+    }
 
-  def genOutput:(Seq[String]) => O
+    def hdr = rule {
+      row
+    }
 
-  def makeListOfOutputValues = (h: O, r: Seq[O]) => h::(r.toList:List[O])
+    def row = rule {
+      oneOrMore(field).separatedBy(",") ~> genOutput ~ optional("\r") ~ "\n"
+    }
 
-  /* parser action */
-  def makeText: (String) => String
-  def makeString: (String) => String
-  def makeEmpty: () => String
+    def field = rule {
+      string | text | MATCH ~> makeEmpty
+    }
 
-  def name:String
+    def text = rule {
+      clearSB() ~ oneOrMore(noneOf(",\"\n\r") ~ appendSB()) ~ push(sb.toString) ~> makeText
+    }
+
+    def string = rule {
+      WS ~ "\"" ~ clearSB() ~ zeroOrMore(("\"\"" | noneOf("\"")) ~ appendSB()) ~ push(sb.toString) ~> makeString ~ "\"" ~ WS
+    }
+
+    val whitespace = CharPredicate(" \t")
+
+    def WS = rule {
+      zeroOrMore(whitespace)
+    }
+
+    def genOutput: (Seq[String]) => O
+
+    def makeListOfOutputValues = (h: O, r: Seq[O]) => h :: (r.toList: List[O])
+
+    /* parser action */
+    def makeText: (String) => String
+
+    def makeString: (String) => String
+
+    def makeEmpty: () => String
+
+    def name: String
+  }
+
+  trait CSVParserAction {
+    // remove leading and trailing blanks
+    def makeText = (text: String) => text.trim
+
+    // replace sequence of two double quotes by a single double quote
+    def makeString = (string: String) => string.replaceAll("\"\"", "\"")
+
+    // modify result of EMPTY token if required
+    def makeEmpty = () => ""
+  }
+
+  trait CSVParserIETFAction extends CSVParserAction {
+    // no trimming of WhiteSpace
+    override def makeText = (text: String) => text
+  }
+
 }
 
-trait CSVParserAction {
-  // remove leading and trailing blanks
-  def makeText = (text: String) => text.trim
-  // replace sequence of two double quotes by a single double quote
-  def makeString = (string: String) => string.replaceAll("\"\"", "\"")
-  // modify result of EMPTY token if required
-  def makeEmpty = () => ""
-}
-
-trait CSVParserIETFAction extends CSVParserAction {
-  // no trimming of WhiteSpace
-  override def makeText = (text: String) => text
-}
+object CSVParsboiledParsers extends CSVParsboiledParsers
