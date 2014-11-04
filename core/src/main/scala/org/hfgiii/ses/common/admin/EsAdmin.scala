e8f2bf4011116a03a919fad6b11fff7e4f5ec1d9
hgilmore <hgilmore@underarmour.com>
2014-11-04 07:34:55 -0500

    Fixed some problems with life cycle differences between clients to embedded nodes and clients to remote nodes

--- core/src/main/scala/org/hfgiii/ses/common/admin/EsAdmin.scala
+++ core/src/main/scala/org/hfgiii/ses/common/admin/EsAdmin.scala
@@ -2,6 +2,7 @@ package org.hfgiii.ses.common.admin
 
 import com.sksamuel.elastic4s.ElasticClient
 import org.elasticsearch.common.settings.ImmutableSettings
+import org.elasticsearch.node.NodeBuilder._
 import com.sksamuel.elastic4s.ElasticDsl._
 
 trait EsAdmin {
@@ -9,20 +10,33 @@ trait EsAdmin {
   def initLocalEs4sClient(settings:ImmutableSettings.Builder):ElasticClient =
      ElasticClient.local(settings.build)
 
+
+  def initLocalEs4sClient:ElasticClient = {
+    val node = nodeBuilder.data(false).local(false).node
+
+    ElasticClient.fromNode(node)
+  }
+
+
   def initRemoteEs4sClient(settings:ImmutableSettings.Builder,addresses:(String,Int)*):ElasticClient =
     ElasticClient.remote(settings.build,addresses: _*)
 
-  def shutdownElasticsearch(implicit client:ElasticClient) {
+  def shutdownElasticsearch(implicit client:ElasticClient) =
     client.shutdown.await
-  }
+
+  def closeElasticsearchClient(implicit client:ElasticClient) =
+      client.close
 
   def emptyClient:ElasticClient = ElasticClient.local
 
-  def withES(ex: => Unit)(implicit client:ElasticClient): Unit = {
+  def withES(remote:Boolean)(ex: => Unit)(implicit client:ElasticClient): Unit = {
 
     ex
 
-    shutdownElasticsearch
+    if(remote)
+       closeElasticsearchClient
+    else
+       shutdownElasticsearch
 
   }
 
